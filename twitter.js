fde0f1850973b69d690d465ea488e690776ef570
Jaime Cavazos <jaimejcavazos@gmail.com>
2017-01-29 01:13:18 -0600

    Added new mongodb string for conneting

--- twitter.js
+++ twitter.js
@@ -27,7 +27,9 @@ var dbs = {};
 
 
 //connect away
-MongoClient.connect('mongodb://127.0.0.1:27017/twitter', function(err, db) {
+
+//MongoClient.connect('mongodb://127.0.0.1:27017/twitter
+MongoClient.connect('mongodb://heroku_9sw52m8z:kill129$999@ds135969-a0.mlab.com:35969,ds135969-a1.mlab.com:35969/heroku_9sw52m8z?replicaSet=rs-ds135969', function(err, db) {
     if (err) throw err;
     console.log("Connected to Database");
 
