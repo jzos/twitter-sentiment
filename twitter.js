5de8a331e7522d13e1fc31126dd6c0b9a26c1f0c
Jaime Cavazos <jaimejcavazos@gmail.com>
2017-01-29 13:50:18 -0600

    Added Procfile

--- twitter.js
+++ twitter.js
@@ -9,7 +9,7 @@ var express             = require('express');
 var app = express();
 
 
-app.listen(process.env.PORT || 5000);
+app.set('port', (process.env.PORT || 5000));
 
 
 var Twit = require('twit');
