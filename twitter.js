ade01433753a5dd2780fca908a079e6f96c5904d
Jaime Cavazos <jaimejcavazos@gmail.com>
2017-01-29 03:34:18 -0600

    updated twitter.js file

--- twitter.js
+++ twitter.js
@@ -19,6 +19,8 @@ var T = new Twit({
 })
 
 
+/*
+
 //require node modules (see package.json)
 var MongoClient = require('mongodb').MongoClient
     , format = require('util').format;
@@ -26,8 +28,11 @@ var MongoClient = require('mongodb').MongoClient
 
 var dbs = {};
 
+*/
 
 //connect away
+
+/*
 
 //MongoClient.connect('mongodb://127.0.0.1:27017/twitter', function(err, db) {
 MongoClient.connect('mongodb://mongoapp:kill129999@ds135969-a0.mlab.com:35969,ds135969-a1.mlab.com:35969/?replicaSet=rs-ds135969', function(err, db) {
@@ -36,6 +41,8 @@ MongoClient.connect('mongodb://mongoapp:kill129999@ds135969-a0.mlab.com:35969,ds
 
     dbs = db;
 });
+
+*/
 
 
 /*
@@ -270,13 +277,14 @@ stream.on('tweet', function (tweet) {
     var dataRecord      = merge(tweet,r1);
 
     console.log(dataRecord);
+    /*
 
     //insert record
     dbs.collection('jackinthebox').insert(dataRecord, function(err, records) {
         if (err) throw err;
         console.log("Record added as "+records.ops[0]._id);
     });
-
+     */
 
     console.log("*************************************");
 
