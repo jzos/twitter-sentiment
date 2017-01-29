2caebbfcfb444958c0f3d604cb3d693b82b0df8f
Jaime Cavazos <jaimejcavazos@gmail.com>
2017-01-29 13:37:55 -0600

    Updated twitter.js

--- twitter.js
+++ twitter.js
@@ -9,7 +9,7 @@ var express             = require('express');
 var app = express();
 
 
-app.set('port', (process.env.PORT || 5000));
+app.listen(process.env.PORT || 5000);
 
 
 var Twit = require('twit');
