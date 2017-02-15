9b0e22ae3508a2261c910b9636f223423219ca6b
Jaime Cavazos <jaimejcavazos@gmail.com>
2017-02-14 22:04:31 -0600

    Added automotive.js file for collecting automotive data

---
+++ automotive.js
@@ -0,0 +1,89 @@
+/**
+ * Created by jaimemac on 12/4/16.
+ */
+
+var sentiment           = require('sentiment');
+var merge               = require('merge'), original, cloned;
+
+//var express             = require('express');
+//var app = express();
+//app.set('port', (process.env.PORT || 5000));
+
+
+var Twit = require('twit');
+
+var T = new Twit({
+    consumer_key: 'qphEVi498C5OGVaCKnl6QGVGy',
+    consumer_secret: '9TKbkrcVSRqefw5fQ9v8wkQtktbHOSjdy2evUzwSxjylxqACF4',
+    access_token: '19669840-EE31VHbTfidEHEhqnO2epnsaY6EqSy6XUbMBfRjkg',
+    access_token_secret: 'MyzxhVfaZKEwwIGqB6zZdMaE0zIGJsWt6q2e5WWB4QnRX',
+    timeout_ms:           6000*1000  // optional HTTP request timeout to apply to all requests.
+})
+
+
+//require node modules (see package.json)
+var MongoClient = require('mongodb').MongoClient
+    , format = require('util').format;
+
+
+var dbs = {};
+
+
+//connect away
+
+
+
+MongoClient.connect('mongodb://127.0.0.1:27017/automotive', function(err, db) {
+//MongoClient.connect('mongodb://mongoapp:kill129999@ds135969-a0.mlab.com:35969,ds135969-a1.mlab.com:35969/heroku_9sw52m8z?replicaSet=rs-ds135969', function(err, db) {
+    if (err) throw err;
+    console.log("Connected to Database");
+
+    dbs = db;
+});
+
+
+
+
+
+T.get('account/verify_credentials', { skip_status: true })
+    .catch(function (err) {
+        console.log('caught error', err.stack)
+    })
+    .then(function (result) {
+        // `result` is an Object with keys "data" and "resp".
+        // `data` and `resp` are the same objects as the ones passed
+        // to the callback.
+        // See https://github.com/ttezel/twit#tgetpath-params-callback
+        // for details.
+
+        //console.log('data', result.data);
+    })
+
+
+sAutomotive = 'Capitol Chevrolet,Covert Chevrolet,Auto Nation Chevrolet,henna Chevrolet,"rush chevrolet","covert ford","maxwell ford","truck city ford","south point dodge","nyle maxwell","covert dodge","Charles Maund Toyota","AutoNation Toyota","Round Rock Toyota",Roger Beasley Mazda,"South Point Hyundai","Howdy Honda",""Town North Nissan","dream cars austin"';
+
+
+
+var stream = T.stream('statuses/filter', { track: sAutomotive, language: 'en' });
+
+
+
+stream.on('tweet', function (tweet) {
+
+    var r1              = sentiment(tweet.text);
+    var dataRecord      = merge(tweet,r1);
+
+    console.log(dataRecord);
+
+    //insert record
+    dbs.collection('dealerships').insert(dataRecord, function(err, records) {
+        if (err) throw err;
+        console.log("Record added as "+records.ops[0]._id);
+    });
+
+
+    console.log("*************************************");
+
+})
+
+
