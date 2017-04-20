f3a311b9fcdfbaa63391e3657abaaf7a6632dc4d
Jaime Cavazos <jaimejcavazos@gmail.com>
2017-04-20 16:26:38 -0500

    Changed URL for customer service request

--- app/customer-service/customerService.controller.js
+++ app/customer-service/customerService.controller.js
@@ -17,7 +17,7 @@
 
         function loadComplaintStream()
         {
-            RequestAPI.loadURL("http://localhost:5005/api/complaint-stream", populateComplaintStream, failureCallback);
+            RequestAPI.loadURL("http://platform.symamp.com:5005/api/complaint-stream", populateComplaintStream, failureCallback);
             Utilities.watchBinding("populateComplaintStream", populateComplaintStream, 1000, $scope, $timeout);
         }
 
