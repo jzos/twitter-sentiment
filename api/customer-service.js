1c112d85d8b21cb6edad8af30e4789ba086f631d
Jaime Cavazos <jaimejcavazos@gmail.com>
2017-04-23 15:08:06 -0500

    Added compression and limited fields for the customer-service api

--- api/customer-service.js
+++ api/customer-service.js
@@ -4,6 +4,7 @@
 
 
 var express = require('express');
+var compression = require('compression')
 
 var app = express();
 app.set('port', process.env.PORT || 5005);
@@ -13,6 +14,7 @@ app.listen(app.get('port'), function(){
     console.log('Express server listening on port ' + app.get('host') + ':' + app.get('port'));
 });
 
+app.use(compression())
 
 
 var MongoClient         = require('mongodb').MongoClient
@@ -60,7 +62,7 @@ app.get('/api/complaint-stream', function (req, res, next) {
 
     //find records
     dbs.collection('dealerships', function(err, collection) {
-        collection.find({},{created_at: 1, text: 1, coordinates: 1, score: 1, data_source: 1, user: 1, postURL: 1}).limit(200).sort({"created_at": -1}).toArray(function(err, items) {
+        collection.find({},{created_at: 1, text: 1, coordinates: 1, score: 1, data_source: 1, 'user.profile_image_url': 1,'user.name': 1, 'user.screen_name': 1, postURL: 1}).limit(200).sort({"created_at": -1}).toArray(function(err, items) {
             console.log(items);
             res.send(req.query.callback + '('+ JSON.stringify(items) + ');');
         });
